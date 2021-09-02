603dc2e6c83d8fd81b414528f4813eb8bf9e133c
Driccioli <driccioli53@gmail.com>
2021-09-02 16:24:22 +0200

    Errore con Laravel Mix

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<div class="header-1">
+    <ul>
+        <li>DC POWER™ VISA©</li>
+        <li>ADDITIONAL DC SITES</li>
+    </ul>
+</div>
+
+<div class="header-2">
+    <div class="logo-container">
+        <img src="/images/dc-logo.png" alt="DC Logo">
+    </div>
+    <nav>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+    </nav>
+</div>
